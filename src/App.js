c2715136c19a8902efb246faee18b0d7852a9add
zack <zackzbradshaw@gmail.com>
2022-04-06 21:51:44 -0500

    changed browser router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import "./App.css";
 
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Cart from "./components/Cart";
 import Home from "./components/Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" exact element={<Home />}></Route>
         <Route path="/cart" exact element={<Cart />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
